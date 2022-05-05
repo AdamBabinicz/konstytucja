4658e8866a715f64e103c25d29dcd8b1dfaaab68
babinicz <babinicz@onet.pl>
2022-05-05 14:47:54 +0200

    google analitics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Project from "./routes/Project";
 import Contact from "./routes/Contact";
 import { Routes, Route } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
@@ -16,6 +17,33 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </>
   );
 }
